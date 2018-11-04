8c949a7a41425da1070d29d651a5ce6069606e0d
Nuno Ramos <nunor07.unfc@gmail.com>
2018-11-04 18:58:43 +0000

    Defining main layout with bottom navigation view

--- app/src/main/java/com/example/nuno/tickets_payment_client/MainActivity.java
+++ app/src/main/java/com/example/nuno/tickets_payment_client/MainActivity.java
@@ -3,31 +3,34 @@ package com.example.nuno.tickets_payment_client;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
+            Fragment selectedFragment = null;
+
+            switch (item .getItemId()) {
+                case R.id.navigation_tickets:
+                    selectedFragment = new TicketsFragment();
+                    break;
+                case R.id.navigation_cafetaria:
+                    selectedFragment = new CafetariaFragment();
+                    break;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+                    selectedFragment = new TicketsFragment();
+                    break;
             }
-            return false;
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+            return true;
         }
     };
 
@@ -36,9 +39,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new TicketsFragment()).commit();
     }
 
 }
