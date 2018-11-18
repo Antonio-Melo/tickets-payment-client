6524580791970fcea4ebc80c3d189f98de60d7dc
Nuno Ramos <nunor07.unfc@gmail.com>
2018-11-18 12:24:04 +0000

    Generating qr code

--- app/src/main/java/com/example/nuno/tickets_payment_client/TicketActivity.java
+++ app/src/main/java/com/example/nuno/tickets_payment_client/TicketActivity.java
@@ -1,14 +1,30 @@
 package com.example.nuno.tickets_payment_client;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.nuno.tickets_payment_client.logic.Show;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+import static android.graphics.Color.BLACK;
+import static android.graphics.Color.WHITE;
 
 public class TicketActivity extends AppCompatActivity {
 
+    public final static int WHITE = 0xFFFFFFFF;
+    public final static int BLACK = 0xFF000000;
+    public final static int WIDTH = 800;
+    public final static int HEIGHT = 800;
+
     private Show show;
+
+    private ImageView qrCodeImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +37,37 @@ public class TicketActivity extends AppCompatActivity {
         ((TextView)findViewById(R.id.show_artist_text_view)).setText(show.getArtist());
         ((TextView)findViewById(R.id.show_date_text_view)).setText(show.getDate());
 
+        qrCodeImageView = findViewById(R.id.ticket_qr_code);
+        try {
+            Bitmap bitmap = encodeAsBitmap("boas tardes");
+            qrCodeImageView.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    Bitmap encodeAsBitmap(String str) throws WriterException {
+        BitMatrix result;
+        try {
+            result = new MultiFormatWriter().encode(str, BarcodeFormat.QR_CODE, WIDTH, HEIGHT, null);
+        } catch (IllegalArgumentException iae) {
+            // Unsupported format
+            return null;
+        }
+
+        int width = result.getWidth();
+        int height = result.getHeight();
+        int[] pixels = new int[width * height];
+        for (int y = 0; y < height; y++) {
+            int offset = y * width;
+            for (int x = 0; x < width; x++) {
+                pixels[offset + x] = result.get(x, y) ? BLACK : WHITE;
+            }
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+        return bitmap;
     }
 }
